0168d5c1ad43bee2de70ee12696bdb96f4869433
IsmaCHup <annym.gg@mail.ru>
2022-09-21 17:02:51 +0300

    add connect server and dir

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json);
+
+mongoose
+    .connect("mongodb+srv://Is:lam@cluster0.7eomugg.mongodb.net/books")
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Сервер успешно запущен");
+        });
+    })
+    .catch((e) => console.log(e.message));
